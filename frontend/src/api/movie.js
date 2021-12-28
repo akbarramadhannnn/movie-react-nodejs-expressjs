8e2b6ff1657a2c025446524b588e231a9222888c
akbarramadhannnn <akbarramadhan452@gmail.com>
2021-12-28 23:49:26 +0700

    update config api

--- frontend/src/api/movie.js
+++ frontend/src/api/movie.js
@@ -1,20 +1,17 @@
 import axios from "axios";
+import URL_API from "../config/url";
 
 export const ApiGetMovie = async (search) => {
-  const result = await axios.get(
-    `http://localhost:2021/api/movie?search=${search}`
-  );
+  const result = await axios.get(`${URL_API}/movie?search=${search}`);
   return result.data;
 };
 
 export const ApiAddMovie = async (payload) => {
-  const result = await axios.post(`http://localhost:2021/api/movie`, payload);
+  const result = await axios.post(`${URL_API}/movie`, payload);
   return result.data;
 };
 
 export const ApiDeleteMovie = async (movieId) => {
-  const result = await axios.delete(
-    `http://localhost:2021/api/movie/${movieId}`
-  );
+  const result = await axios.delete(`${URL_API}/movie/${movieId}`);
   return result.data;
 };
---
+++ frontend/src/config/url.js
@@ -0,0 +1,6 @@
+const URL_API_LOCAL = "http://localhost:2021/api";
+const URL_API_PROD = "https://akbarramadhan.com/api";
+
+export default process.env.NODE_ENV === "development"
+  ? URL_API_LOCAL
+  : URL_API_PROD;
